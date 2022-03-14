d892c488ec5ec329b4ff71d477b9b2905c65e9e8
ali <alimartondi88@gmail.com>
2022-03-14 19:27:32 +0700

    Add hero and footer section

---
+++ resources/views/home.blade.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="../style/utilities.css">
+
+  <title>Good Food Jakarta | Home </title>
+</head>
+
+<body>
+  {{-- Header --}}
+  <header class="fixed-top">
+    <div class="container">
+      <nav class="py-5">
+        <div
+          class="humberger-menu rounded-circle h-40 w-40 bg-white d-flex justify-content-center align-items-center opacity-75 mb-3 shadow">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-list"
+            viewBox="0 0 16 16">
+            <path fill-rule="evenodd"
+              d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+          </svg>
+        </div>
+        <div
+          class="profile rounded-circle h-40 w-40 bg-white d-flex justify-content-center align-items-center opacity-75 shadow">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person"
+            viewBox="0 0 16 16">
+            <path
+              d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+          </svg>
+        </div>
+      </nav>
+    </div>
+  </header>
+  {{-- End of Header --}}
+  <main>
+    <div class="container-xxl px-0">
+      {{-- Hero --}}
+      <section class="vh-100 hero position-relative">
+        <div class="position-absolute position-absolute top-50 start-50 translate-middle z-index-3">
+          <img src="../images/goodfood-logo.png" alt="goodfood logo">
+        </div>
+        <div class="d-flex bg-info justify-content-center align-items-center w-100 h-100 overflow-hidden">
+          <div class="bg-dark opacity-50 position-absolute top-0 bottom-0 start-0 end-0"></div>
+          <img src="../images/hero.png" alt="hero image">
+        </div>
+        <div class="position-absolute top-100 start-50 translate-middle">
+          <img src="../images/frame.svg" alt="frame">
+        </div>
+      </section>
+      {{-- End of Hero --}}
+      <section class="quoete mt-10">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-9 border p-5">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur praesentium possimus voluptate
+                voluptatem iste est quae quibusdam? Fugiat, atque, aspernatur aperiam provident alias, error eveniet
+                architecto placeat praesentium modi nihil!</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat iure iusto labore quisquam, vero atque
+                nam quos libero vel architecto, accusamus in nesciunt ex perspiciatis a nemo et autem sapiente non
+                voluptatum quibusdam ullam repudiandae. Veritatis molestiae consequuntur saepe quae neque, earum porro
+                corrupti iure quam tempora! Eos, nulla quasi.</p>
+            </div>
+          </div>
+      </section>
+    </div>
+  </main>
+
+  <footer class="bg-main-purple py-5 mt-10">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="">
+            <img src="../images/good-footer-logo.svg" alt="good food logo">
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <ul class="list-unstyled">
+            <li>
+              <a href="#" class="text-uppercase text-white">About</a>
+            </li>
+            <li>
+              <a href="#" class="text-uppercase text-white">Good Food Everyday</a>
+            </li>
+            <li>
+              <a href="#" class="text-uppercase text-white">Good Food Catering</a>
+            </li>
+            <li>
+              <a href="#" class="text-uppercase text-white">Cuisines</a>
+            </li>
+            <li>
+              <a href="#" class="text-uppercase text-white">FAQ</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-lg-3 d-flex flex-column">
+          <a href="#" class="text-decoration-none mb-3">
+            <img src="../images/facebook-icon.svg" alt="facebook icon">
+          </a>
+          <a href="#" class="text-decoration-none">
+            <img src="../images/instagram-icon.svg" alt="instagram icon">
+          </a>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Optional JavaScript; choose one of the two! -->
+
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+  </script>
+
+  <!-- Option 2: Separate Popper and Bootstrap JS -->
+  <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
