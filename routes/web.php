94b850fb162651ee25e0c8a1d9f2630d80770faa
Flourish <98024174+Fllourish@users.noreply.github.com>
2022-03-18 13:45:41 +0700

    Revert "struktur page"

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
 Route::get('/about', function () {
     return view('about/index');
-});
-
-Route::get('/menu', function () {
-    return view('menu/index');
 });
 
 Route::get('/contact', function () {
