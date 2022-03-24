f0eef45fb4ddaec27e9ed04ab65cf706648249d4
Milla Fitriyany <58808461+millaf1222@users.noreply.github.com>
2022-03-24 15:17:22 +0700

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,54 +1,43 @@
 <footer class="bg-main-purple py-5 mt-10">
     <div class="container">
-        <div class="row">
-            <div class="col-lg-6">
-                <div class="">
-                    <img src="../images/good-footer-logo.svg" alt="good food logo">
-                </div>
-            </div>
-            <div class="col-lg-3">
-                <ul class="list-unstyled">
-                    <li>
-                        <a href="#" class="text-uppercase text-white">About</a>
-                    </li>
-                    <li>
-                        <a href="#" class="text-uppercase text-white">Good Food Everyday</a>
-                    </li>
-                    <li>
-                        <a href="#" class="text-uppercase text-white">Good Food Catering</a>
-                    </li>
-                    <li>
-                        <a href="#" class="text-uppercase text-white">Cuisines</a>
-                    </li>
-                    <li>
-                        <a href="#" class="text-uppercase text-white">FAQ</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-lg-3 d-flex flex-column">
-                <a href="#" class="text-decoration-none mb-3">
-                    <img src="../images/facebook-icon.svg" alt="facebook icon">
-                </a>
-                <a href="#" class="text-decoration-none">
-                    <img src="../images/instagram-icon.svg" alt="instagram icon">
-                </a>
-            </div>
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="">
+            <a href="/" class="text-decoration-none mb-3">
+                <img src="../images/good-footer-logo.svg" alt="good food logo">
+              </a>
+          </div>
+          <div class="footer-copyright">
+            <p>&copy; Copyrights 2022</p>
         </div>
+        </div>
+        <div class="col-lg-3">
+          <ul class="list-unstyled">
+            <li>
+              <a href="/about" class="text-uppercase text-white">About</a>
+            </li>
+            <li>
+              <a href="/everyday" class="text-uppercase text-white">Good Food Everyday</a>
+            </li>
+            <li>
+              <a href="/catering" class="text-uppercase text-white">Good Food Catering</a>
+            </li>
+            <li>
+              <a href="#" class="text-uppercase text-white">Cuisines</a>
+            </li>
+            <li>
+              <a href="/faq" class="text-uppercase text-white">FAQ</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-lg-3 d-flex flex-column">
+          <a href="#" class="text-decoration-none mb-3">
+            <img src="../images/facebook-icon.svg" alt="facebook icon">
+          </a>
+          <a href="#" class="text-decoration-none">
+            <img src="../images/instagram-icon.svg" alt="instagram icon">
+          </a>
+        </div>
+      </div>
     </div>
-</footer>
-
-<!-- Optional JavaScript; choose one of the two! -->
-
-<!-- Option 1: Bootstrap Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-</script>
-
-<!-- Option 2: Separate Popper and Bootstrap JS -->
-<!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    -->
-</body>
-
-</html>
+  </footer>
